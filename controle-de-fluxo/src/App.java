1e77ab00cb7a90489f516e468c40aefa8405dab5
pedroGuimaSena <43865787+pedroGuimaSena@users.noreply.github.com>
2024-09-17 14:39:31 -0300

    Exemplo de utilização de repetição com for.

--- controle-de-fluxo/src/App.java
+++ controle-de-fluxo/src/App.java
@@ -1,7 +1,19 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        // EXEMPLO DE UTILIZAÇÃO DE FOR:
+    
+    /* 
+    for (bloco de inicialização; expressão booleana de validação; bloco de atualização)
+    {
+        // comando que será executado até que a 
+        // expressão de validação torne-se falsa 
+    }
+    */
 
+        // EXEMPLO DE UTILIZAÇÃO DE FOR:
+        for (int carneirinhos = 0; carneirinhos <= 30; carneirinhos++)
+        {
+         System.err.println(carneirinhos + " - Carneirinho(s)");   
+        }
         
 
     }
